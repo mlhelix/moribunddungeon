8e44e0999653ffc3a87325a375c4a699e7cca11f
marquis harden <marquis.harden.07@cnu.edu>
2026-02-23 13:23:26 -0500

    Scene creation for Title and Options
    Made Scenes for Title and Options menus. Exit button works, change scene title and options works.

---
+++ Scripts/title_screen.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	#get_tree().change_scene_to_file()
+	pass # Replace with function body.
+
+
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
+	pass # Replace with function body.
+
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
