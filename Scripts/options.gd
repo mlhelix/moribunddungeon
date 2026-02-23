82552e9d18763f3073b461f4feb9d14602826957
marquis harden <marquis.harden.07@cnu.edu>
2026-02-23 14:43:46 -0500

    Added Keyboard input

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -1,7 +1,9 @@
 extends Control
 
-
-
+func _process(delta):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
+	pass
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
