26209f12ac7f6b419d235a75a3f6ec4d6ca3d356
marquis harden <marquis.harden.07@cnu.edu>
2026-02-25 11:15:42 -0500

    Tested exporting to exe

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -8,3 +8,8 @@ extends Control
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
 	pass # Replace with function body.
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
+	pass # Replace with function body.
