b0805b56929292d6d39cd79ef78f61e45ebe3bcb
marquis harden <marquis.harden.07@cnu.edu>
2026-04-13 03:46:39 -0400

    INV image fixes

--- Scripts/player_inventory_slot.gd
+++ Scripts/player_inventory_slot.gd
@@ -8,6 +8,6 @@ func update(item: Item):
 	else:
 		itemSprite.visible = true
 		itemSprite.texture = item.sprite
-		var scaling = Vector2($".".size.x / itemSprite.texture.get_width(), $".".size.y / itemSprite.texture.get_height())
+		var scaling = Vector2(($".".size.x / itemSprite.texture.get_width()) - 0.02, ($".".size.y / itemSprite.texture.get_height()) - 0.02)
 		itemSprite.apply_scale(Vector2(scaling))
 	
