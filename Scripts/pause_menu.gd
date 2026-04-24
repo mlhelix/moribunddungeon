812bca5dcf3a2ad46ec628e2943379d4ac01da61
marquis harden <marquis.harden.07@cnu.edu>
2026-04-24 14:49:40 -0400

    heal on exit

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -53,6 +53,7 @@ func _on_settings_pressed() -> void:
 
 
 func _on_quit_pressed() -> void:
+	GlobalManager.stats.heal_damage(100)
 	$AnimationPlayer.play("RESET")
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
